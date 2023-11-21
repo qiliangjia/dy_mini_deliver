d505902ff774592b213920291e352f429b493c2e
JaeLiang <1771332041@qq.com>
2023-11-21 14:41:10 +0800

    计算

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,3 +1,5 @@
+var calc = require("../../utils/calc")
+
 Page({
   data: {
     tabList: [{
@@ -25,9 +27,97 @@ Page({
       '短视频内容创作数据服务',
     ],
     productList: ["官网小程序(参考图)", "电商(参考图)", "茶铺商城(参考图)", "计时器功能(参考图)"],
-    tousu: ''
+    name: '',
+    phone: '',
+    company: '',
+    calc: {},
+    tapped: {}
   },
-  onLoad(options) {},
+  onLoad(options) {
+    calc.reset()
+  },
+  showAbout: function (e) {
+    wx.showModal({
+      title: '关于',
+      content: '一个简单的计算器 @V1.0',
+      showCancel: false
+    })
+  },
+  btnClicked: function (e) {
+    var code = e.target.dataset.op
+    calc.addOp(code)
+    console.log(calc.getVars())
+    this.setData({
+      calc: calc.getVars()
+    })
+  },
+  btnTouchStart: function (e) {
+    var code = e.target.dataset.op
+    var tapped = {
+      [code]: 'active'
+    }
+    this.setData({
+      tapped: tapped
+    })
+  },
+  btnTouchEnd: function (e) {
+    var code = e.target.dataset.op
+    var tapped = {}
+    this.setData({
+      tapped: tapped
+    })
+  },
+  nameInput(e) {
+    this.setData({
+      name: e.detail.value.trim()
+    })
+  },
+  phoneInput(e) {
+    this.setData({
+      phone: e.detail.value.trim()
+    })
+  },
+  companyInput(e) {
+    this.setData({
+      company: e.detail.value.trim()
+    })
+  },
+  sumbit() {
+    const name = this.data.name
+    const phone = this.data.phone
+    const company = this.data.company
+    const phoneRegex = /^1\d{10}$/;
+    if (!name) {
+      tt.showToast({
+        title: '姓名不能为空',
+        icon: 'fail'
+      });
+    } else if (!phone) {
+      tt.showToast({
+        title: '手机号不能为空',
+        icon: 'fail'
+      });
+    } else if (!phoneRegex.test(phone)) {
+      tt.showToast({
+        title: '请输入正确的手机号',
+        icon: 'fail'
+      });
+    } else if (!company) {
+      tt.showToast({
+        title: '公司不能为空',
+        icon: 'fail'
+      });
+    } else {
+      tt.showLoading({
+        title: '提交中...',
+      });
+      setTimeout(() => {
+        tt.redirectTo({
+          url: '/pages/success/index',
+        });
+      }, 1500);
+    }
+  },
   jumptoPay() {
     tt.navigateTo({
       url: '/pages/home1/index',
---
+++ pages/success/index.js
@@ -0,0 +1,9 @@
+// /Users/macbook/mac/dy_miniapp/dy_mini_deliver/pages/success/index.js
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+
+  }
+})
