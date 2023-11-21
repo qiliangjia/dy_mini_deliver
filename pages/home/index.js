a09e5ef9744a4d0843d70c262acb4621699859b4
JaeLiang <1771332041@qq.com>
2023-11-21 15:08:15 +0800

    ad广告

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,14 +1,39 @@
 Page({
-  data: {
-
-  },
+  data: {},
   onLoad: function (options) {
-
+    this.setAd()
+  },
+  setAd() {
+    this.ad = tt.createRewardedVideoAd({
+      adUnitId: "f04kksnbn2vf9w76ov",
+    });
+    this.ad.onClose((data) => {
+      console.log(data);
+      tt.hideLoading();
+      if (data.isEnded) {
+        tt.showToast({
+          title: '解锁成功！',
+          icon: 'success'
+        });
+        setTimeout(() => {
+          tt.navigateTo({
+            url: '/pages/detail/index',
+          });
+        }, 800);
+      } else {
+        tt.showToast({
+          title: '解锁失败！',
+          icon: 'fail'
+        });
+      }
+    });
+    this.ad.load();
   },
   jumptoButton() {
-    tt.navigateTo({
-      url: '/pages/detail/index',
+    tt.showLoading({
+      title: '视频加载中',
     });
+    this.ad.show();
   },
   onShow() {
 
